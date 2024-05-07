ec7cc6cf72212addc0c3584c58336c4ee4dd8c63
lharnist <lharnist@ucsc.edu>
2024-05-06 23:02:02 -0700

    Changed the spawnrates, gave 3 lives, added sounds effects and made it restartable, made the enemies properly dissapear.

--- src/Scenes/testScene.js
+++ src/Scenes/testScene.js
@@ -8,6 +8,11 @@ class testScene extends Phaser.Scene {
         this.my.sprite.bullet = [];
         this.my.sprite.enemyBullets = [];
         this.maxBullets = 4;
+        this.my.sprite.enemyArray = [];
+        this.my.score = 0;
+        this.my.scoretext;
+        this.my.lives = 3;
+        this.my.livestext;
 
     }
 
@@ -23,18 +28,46 @@ class testScene extends Phaser.Scene {
         this.load.image("pinkExplosion", "assets/laserPink_burst.png");
         this.load.image("blueExplosion", "assets/laserBlue_burst.png");
         this.load.image("greenExplosion", "assets/laserGreen_burst.png");
-
-
+        this.load.audio("greenlaser", "assets/laserLarge_000.ogg")
+        this.load.audio("pinklaser", "assets/laserLarge_001.ogg")
+        this.load.audio("bluelaser", "assets/laserLarge_002.ogg")
+        this.load.audio("explosion", "assets/Explosion_Sound.mp3")
     }
 
     create() {
         let my = this.my;
+
+        this.points1 = [
+            292, 212,
+            450,   594,
+            800, 300,
+            1200, 300,
+            292, 212
+        ];
+
+        this.points2 = [
+            773, 115,
+            590, 501,
+            790, 521,
+            1550, 115,
+            773, 115
+        ];
+
+        this.path1 = new Phaser.Curves.Spline(this.points1);
+        this.path2 = new Phaser.Curves.Spline(this.points2);
+
         my.sprite.player = this.add.sprite(500, 750, "player");
         my.sprite.enemyPink1 = this.add.follower(this.path2, 450, 150, "enemyPink");
-        my.sprite.enemyPink2 = this.add.sprite(550, 150, "enemyPink");
-        my.sprite.enemyPink3 = this.add.sprite(350, 150, "enemyPink");
-        my.sprite.enemyPink4 = this.add.sprite(250, 150, "enemyPink");
-        my.sprite.enemyPink5 = this.add.sprite(650, 150, "enemyPink");
+        my.sprite.enemyPink2 = this.add.follower(this.path2, 550, 150, "enemyPink");
+        my.sprite.enemyPink3 = this.add.follower(this.path2, 350, 150, "enemyPink");
+        my.sprite.enemyPink4 = this.add.follower(this.path2, 250, 150, "enemyPink");
+        my.sprite.enemyPink5 = this.add.follower(this.path2, 650, 150, "enemyPink");
+        //console.log(typeof(my.sprite.enemyPink1));
+        my.sprite.enemyArray.push(my.sprite.enemyPink1);
+        my.sprite.enemyArray.push(my.sprite.enemyPink2);
+        my.sprite.enemyArray.push(my.sprite.enemyPink3);
+        my.sprite.enemyArray.push(my.sprite.enemyPink4);
+        my.sprite.enemyArray.push(my.sprite.enemyPink5);
         my.sprite.enemyPink1.setScale(.5);
         my.sprite.enemyPink2.setScale(.5);
         my.sprite.enemyPink3.setScale(.5);
@@ -42,14 +75,18 @@ class testScene extends Phaser.Scene {
         my.sprite.enemyPink5.setScale(.5);
         
         my.sprite.enemyGreen1 = this.add.follower(this.path1, 350, 250, "enemyGreen");
-        my.sprite.enemyGreen2 = this.add.sprite(450, 250, "enemyGreen");
-        my.sprite.enemyGreen3 = this.add.sprite(550, 250, "enemyGreen");
+        my.sprite.enemyGreen2 = this.add.follower(this.path1, 450, 250, "enemyGreen");
+        my.sprite.enemyGreen3 = this.add.follower(this.path1, 550, 250, "enemyGreen");
+        my.sprite.enemyArray.push(my.sprite.enemyGreen1);
+        my.sprite.enemyArray.push(my.sprite.enemyGreen2);
+        my.sprite.enemyArray.push(my.sprite.enemyGreen3);
         my.sprite.enemyGreen1.setScale(.5);
         my.sprite.enemyGreen2.setScale(.5);
         my.sprite.enemyGreen3.setScale(.5);
 
 
         this.aKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
+        this.rKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
         this.dKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
         this.spaceKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
         this.fKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
@@ -87,72 +124,190 @@ class testScene extends Phaser.Scene {
             hideOnComplete: true
         });
 
-        this.points1 = [
-            292, 212,
-            124, 313,
-            787, 460,
-            823, 741,
-            9,   794
-        ];
+        my.sprite.enemyGreen1.startFollow({
+            from: 0,
+            to: 1,
+            delay: 0,
+            duration: 10000,
+            //ease: 'Sine.easeInOut',
+            repeat: -1,
+            yoyo: false,
+            rotateToPath: false,
+            rotationOffset: -90
+        });
 
-        this.points2 = [
-            773, 115,
-            644, 578,
-            348, 299,
-            187, 411,
-            908, 789
-        ];
+        my.sprite.enemyGreen2.startFollow({
+            from: 0,
+            to: 1,
+            delay: 0,
+            duration: 10000,
+            //ease: 'Sine.easeInOut',
+            repeat: -1,
+            yoyo: false,
+            rotateToPath: false,
+            rotationOffset: -90
+        });
 
-        this.path1 = new Phaser.Curves.Spline(this.points1);
-        this.path2 = new Phaser.Curves.Spline(this.points2);
+        my.sprite.enemyGreen3.startFollow({
+            from: 0,
+            to: 1,
+            delay: 0,
+            duration: 10000,
+            //ease: 'Sine.easeInOut',
+            repeat: -1,
+            yoyo: false,
+            rotateToPath: false,
+            rotationOffset: -90
+        });
+
+        my.sprite.enemyPink1.startFollow({
+            from: 0,
+            to: 1,
+            delay: 0,
+            duration: 10000,
+            //ease: 'Sine.easeInOut',
+            repeat: -1,
+            yoyo: false,
+            rotateToPath: false,
+            rotationOffset: -90
+        });
+
+        my.sprite.enemyPink2.startFollow({
+            from: 0,
+            to: 1,
+            delay: 0,
+            duration: 10000,
+            //ease: 'Sine.easeInOut',
+            repeat: -1,
+            yoyo: false,
+            rotateToPath: false,
+            rotationOffset: -90
+        });
+
+        my.sprite.enemyPink3.startFollow({
+            from: 0,
+            to: 1,
+            delay: 0,
+            duration: 10000,
+            //ease: 'Sine.easeInOut',
+            repeat: -1,
+            yoyo: false,
+            rotateToPath: false,
+            rotationOffset: -90
+        });
+
+        my.sprite.enemyPink4.startFollow({
+            from: 0,
+            to: 1,
+            delay: 0,
+            duration: 10000,
+            //ease: 'Sine.easeInOut',
+            repeat: -1,
+            yoyo: false,
+            rotateToPath: false,
+            rotationOffset: -90
+        });
+
+        my.sprite.enemyPink5.startFollow({
+            from: 0,
+            to: 1,
+            delay: 0,
+            duration: 10000,
+            //ease: 'Sine.easeInOut',
+            repeat: -1,
+            yoyo: false,
+            rotateToPath: false,
+            rotationOffset: -90
+        });
+        console.log(my.score);
+        this.my.scoretext = this.add.text(20, 750,'Score: ' + my.score);
+        this.my.livestext = this.add.text(800, 750, 'Lives: ' + my.lives);
+        
     }
 
     update() {
         let my = this.my;
+        if(this.my.lives <= 0){
+            this.add.text(300, 400,'Game over!', { fontSize: 48 });
+            this.add.text(300, 500,'Press R to restart', { fontSize: 38 });
+        }
 
         if (this.aKey.isDown) {
-            my.sprite.player.x -= this.playerSpeed; //this.player.x = this.player.x - 10;
+            if(my.sprite.player.x >= 50){
+                my.sprite.player.x -= this.playerSpeed; //this.player.x = this.player.x - 10;
+            }
         }
         if (this.dKey.isDown) {
-            my.sprite.player.x += this.playerSpeed; //this.player.x = this.player.x + 10;
+            if(my.sprite.player.x <= 950){
+                my.sprite.player.x += this.playerSpeed; //this.player.x = this.player.x + 10;
+            }
         }
 
-        if (this.fKey.isDown){
-            let enemyBullet = this.add.sprite(my.sprite.enemyGreen1.x, my.sprite.enemyGreen1.y - (my.sprite.enemyGreen1.displayHeight/2), "bulletGreen");
-            let enemyBullet2 = this.add.sprite(my.sprite.enemyPink1.x, my.sprite.enemyPink1.y - (my.sprite.enemyPink1.displayHeight/2), "bulletPink");
-            my.sprite.enemyBullets.push(enemyBullet);
-            my.sprite.enemyBullets.push(enemyBullet2);
-            enemyBullet.setScale(0.35);
-            enemyBullet2.setScale(0.35);
-            my.sprite.enemyGreen1.startFollow({
-                from: 0,
-                to: 1,
-                delay: 0,
-                duration: 2000,
-                ease: 'Sine.easeInOut',
-                repeat: -1,
-                yoyo: true,
-                rotateToPath: true,
-                rotationOffset: -90
-            });
-
-            my.sprite.enemyPink1.startFollow({
-                from: 0,
-                to: 1,
-                delay: 0,
-                duration: 2000,
-                ease: 'Sine.easeInOut',
-                repeat: -1,
-                yoyo: true,
-                rotateToPath: true,
-                rotationOffset: -90
-            });
+        if(this.rKey.isDown){
+            this.my.lives = 3;
+            this.my.score = 0;
+            this.scene.restart();
         }
+
+        if(my.sprite.enemyGreen1.x <= 100 && my.sprite.enemyGreen1.x >= 96 ||
+           my.sprite.enemyGreen1.x <= 305 && my.sprite.enemyGreen1.x >= 301 ||
+           my.sprite.enemyGreen1.x <= 605 && my.sprite.enemyGreen1.x >= 601){
+             let enemyGreenBullet1 = this.add.sprite(my.sprite.enemyGreen1.x, my.sprite.enemyGreen1.y - (my.sprite.enemyGreen1.displayHeight/2), "bulletGreen");
+             this.sound.play("greenlaser");
+             my.sprite.enemyBullets.push(enemyGreenBullet1);
+             enemyGreenBullet1.setScale(0.35);
+        }
+
+        if(my.sprite.enemyGreen2.x <= 150 && my.sprite.enemyGreen2.x >= 146 ||
+            my.sprite.enemyGreen2.x <= 355 && my.sprite.enemyGreen2.x >= 351 ||
+            my.sprite.enemyGreen2.x <= 655 && my.sprite.enemyGreen2.x >= 651){
+              let enemyGreenBullet2 = this.add.sprite(my.sprite.enemyGreen2.x, my.sprite.enemyGreen2.y - (my.sprite.enemyGreen2.displayHeight/2), "bulletGreen");
+              this.sound.play("greenlaser");
+              my.sprite.enemyBullets.push(enemyGreenBullet2);
+              enemyGreenBullet2.setScale(0.35);
+         }
+
+         if(my.sprite.enemyGreen3.x <= 200 && my.sprite.enemyGreen3.x >= 196 ||
+            my.sprite.enemyGreen3.x <= 405 && my.sprite.enemyGreen3.x >= 401 ||
+            my.sprite.enemyGreen3.x <= 705 && my.sprite.enemyGreen3.x >= 701){
+              let enemyGreenBullet3 = this.add.sprite(my.sprite.enemyGreen3.x, my.sprite.enemyGreen3.y - (my.sprite.enemyGreen3.displayHeight/2), "bulletGreen");
+              this.sound.play("greenlaser");
+              my.sprite.enemyBullets.push(enemyGreenBullet3);
+              enemyGreenBullet3.setScale(0.35);
+         }
+
+        if(my.sprite.enemyPink1.x <= 300 && my.sprite.enemyPink1.x >= 296 ||
+            my.sprite.enemyPink1.x <= 502 && my.sprite.enemyPink1.x >= 498 ||
+            my.sprite.enemyPink1.x <= 705 && my.sprite.enemyPink1.x >= 699){
+              let enemyPinkBullet1 = this.add.sprite(my.sprite.enemyPink1.x, my.sprite.enemyPink1.y - (my.sprite.enemyPink1.displayHeight/2), "bulletPink");
+              this.sound.play("pinklaser");
+              my.sprite.enemyBullets.push(enemyPinkBullet1);  
+              enemyPinkBullet1.setScale(0.35);
+        }
+
+        if(my.sprite.enemyPink2.x <= 400 && my.sprite.enemyPink2.x >= 396 ||
+            my.sprite.enemyPink2.x <= 502 && my.sprite.enemyPink2.x >= 498 ||
+            my.sprite.enemyPink2.x <= 705 && my.sprite.enemyPink2.x >= 701){
+                let enemyPinkBullet2 = this.add.sprite(my.sprite.enemyPink2.x, my.sprite.enemyPink2.y - (my.sprite.enemyPink2.displayHeight/2), "bulletPink");
+                this.sound.play("pinklaser");
+                my.sprite.enemyBullets.push(enemyPinkBullet2);
+                enemyPinkBullet2.setScale(0.35);
+        }
+
+        if(my.sprite.enemyPink3.x <= 500 && my.sprite.enemyPink3.x >= 496 ||
+            my.sprite.enemyPink3.x <= 602 && my.sprite.enemyPink3.x >= 598 ||
+            my.sprite.enemyPink3.x <= 705 && my.sprite.enemyPink3.x >= 701){
+                let enemyPinkBullet3 = this.add.sprite(my.sprite.enemyPink3.x, my.sprite.enemyPink3.y - (my.sprite.enemyPink3.displayHeight/2), "bulletPink");
+                this.sound.play("pinklaser");
+                my.sprite.enemyBullets.push(enemyPinkBullet3);  
+                enemyPinkBullet3.setScale(0.35);
+         }
 
         if(Phaser.Input.Keyboard.JustDown(this.spaceKey)){
             //console.log("space was read");
             if(my.sprite.bullet.length < this.maxBullets){
                 //console.log("if statement ran");
+                this.sound.play("bluelaser");
                 let playerbullet = this.add.sprite(my.sprite.player.x, my.sprite.player.y-(my.sprite.player.displayHeight/2), "bulletPlayer");
                 my.sprite.bullet.push(playerbullet);
                 playerbullet.setScale(0.35);
@@ -183,12 +338,31 @@ class testScene extends Phaser.Scene {
                 my.sprite.enemyPink5,
             ]) {
                 if(this.collides(enemy, bullet)) {
+                this.sound.play("explosion");
                 this.explosionBlue = this.add.sprite(bullet.x, bullet.y, "blueExplosion").play("explosionBlue");
+                //console.log("This is enemy", enemy);
+                enemy.stopFollow();
+                my.sprite.enemyArray.pop(enemy);
                 bullet.y = -100;
                 enemy.visible = false;
-                enemy.x = -100;
+                //enemy.x = -1000;
+                my.score += 25;
+                
+                if (this.my.scoretext) {
+                    // If it exists, update its text content
+                    this.my.scoretext.setText('Score: ' + my.score);
+                }
+                /*else{
+                    scoretext = this.add.text(500, 500, my.score);
+                }*/
+
+                console.log(my.score);
+                
                 this.explosionBlue.on(Phaser.Animations.Events.ANIMATION_COMPLETE, () => {
                     enemy.visible = true;
+                    enemy.x = Math.random()*300;
+                    enemy.y = -100;
+                    enemy.startFollow(10000);
                     //.x = Math.random()*config.width;
                 }, this);
                 }
@@ -198,15 +372,48 @@ class testScene extends Phaser.Scene {
             //console.log((enemybullet.texture.key));
             if(this.collides(my.sprite.player, enemybullet)){
                 if(enemybullet.texture.key == "bulletGreen"){
+                    this.sound.play("explosion");
                     this.explosionGreen = this.add.sprite(enemybullet.x, enemybullet.y, "greenExplosion").play("explosionGreen");
                     enemybullet.y = 900;
+                    this.my.lives -= 1;
+                    this.my.livestext.setText('Lives: ' + my.lives);
                 }
                 if(enemybullet.texture.key == "bulletPink"){
+                    this.sound.play("explosion");
                     this.explosionPink = this.add.sprite(enemybullet.x, enemybullet.y, "pinkExplosion").play("explosionPink");
                     enemybullet.y = 900;
+                    this.my.lives -= 1;
+                    this.my.livestext.setText('Lives: ' + my.lives);
                 }
             }
         }
+
+        my.sprite.enemyArray = my.sprite.enemyArray.filter(enemy => enemy.visible);
+        
+        
+
+        console.log(this.my.lives);
+
+        for (let enemy of [
+            my.sprite.enemyGreen1,
+            my.sprite.enemyGreen2,
+            my.sprite.enemyGreen3,
+            my.sprite.enemyPink1,
+            my.sprite.enemyPink2,
+            my.sprite.enemyPink3,
+            my.sprite.enemyPink4,
+            my.sprite.enemyPink5,
+        ])
+        if(enemy.x < 0 || enemy.x > 1000){
+            if(enemy.y < -50 || enemy.y > 1000){
+                enemy.y = -10;
+                enemy.startFollow(10000);
+            }
+            enemy.x = Math.random()*900;
+            enemy.startFollow(10000);
+        }
+        
+
     }
 
     collides(a,b){
@@ -214,5 +421,7 @@ class testScene extends Phaser.Scene {
         if(Math.abs(a.y - b.y) > (a.displayHeight/2 + b.displayHeight/2)) return false;
         return true;
     }
+
+    
 
 }
